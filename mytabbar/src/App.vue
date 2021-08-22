491014a93562448cc128554d949411128bb37641
Sonia:)fy <931863235@qq.com>
2021-08-22 13:24:14 +0800

    tabbar added

--- mytabbar/src/App.vue
+++ mytabbar/src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div id="app">
+    <div id="tab-bar">
+      <div class="tab-bar-item">
+        <img src="./assets/img/tabbar/homepage.png" />
+        <div>首页</div></div>
+      <div class="tab-bar-item">
+        <img src="./assets/img/tabbar/category.png" />
+        <div>分类</div></div>
+      <div class="tab-bar-item">
+        <img src="./assets/img/tabbar/cart.png" />
+        <div>购物车</div></div>
+      <div class="tab-bar-item">
+        <img src="./assets/img/tabbar/my.png" />
+        <div>我的</div></div>
+    </div>
+
   </div>
 </template>
 
@@ -8,10 +23,33 @@
 export default {
   name: 'App',
   components: {
+
   }
 }
 </script>
 
 <style>
+@import './assets/css/base.css';
+#tab-bar{
+  height: 49px;
+  background-color: #f6f6f6;
+  display: flex;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+}
+.tab-bar-item{
+  flex: 1;
+  text-align: center;
+  font-size: 14px;
+}
+.tab-bar-item img{
+  width: 22px;
+  height: 22px;
+  margin: 3px;
+  vertical-align: middle;
+}
 
 </style>
